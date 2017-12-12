313339d66790042a592a5cfb93be8cbdaa50c1c8
zjw <zjw@mails.com>
2017-12-12 17:09:24 +0800

    admin test
    由于这个工程是在PC上Django1.11生成的，所以需要改mysite/settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -49,6 +49,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 ROOT_URLCONF = 'mysite.urls'
 
